4158a835d9b1dd888afd74349eaf13518dcd882f
Rayyanir Rosales <rayyanir.1990@gmail.com>
2024-03-29 21:05:48 -0400

    6: implementacion de la ruta para ver todos los articulos

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArticleController;
 
+Route::get('articles', [ArticleController::class,'index'])->name('api.v1.articles.index');
 Route::get('articles/{article}', [ArticleController::class,'show'])->name('api.v1.articles.show');
