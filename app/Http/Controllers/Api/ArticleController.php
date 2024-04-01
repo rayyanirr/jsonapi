dc0b1c78223c3783f1427b5a40178bb61cabcb75
Rayyanir Rosales <rayyanir.1990@gmail.com>
2024-04-01 18:50:56 -0400

    eliminacion de recursos usando la especificacion JSON:API

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Article;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveArticleRequest;
 use App\Http\Resources\ArticleResource;
 use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ArticleController extends Controller
 {
@@ -21,39 +23,27 @@ class ArticleController extends Controller
         return ArticleResource::make($article);
     }
 
-    public function update(Article $article,Request $request){
-
-        $request->validate([
-            'data.attributes.title' => ['required','min:4'],
-            'data.attributes.slug' => ['required'],
-            'data.attributes.content' => ['required'],
-        ]);
-
-        $article->update([
-            'title' => $request->input('data.attributes.title'),
-            'slug' => $request->input('data.attributes.slug'),
-            'content' => $request->input('data.attributes.content'),
-
-        ]);
+    public function store(SaveArticleRequest $request): ArticleResource
+    {
+        $article = Article::create($request->validated());
 
         return ArticleResource::make($article);
     }
-    public function store(Request $request){
 
-        $request->validate([
-            'data.attributes.title' => ['required','min:4'],
-            'data.attributes.slug' => ['required'],
-            'data.attributes.content' => ['required'],
-        ]);
-
-        $article = Article::create([
-
-            'title' => $request->input('data.attributes.title'),
-            'slug' => $request->input('data.attributes.slug'),
-            'content' => $request->input('data.attributes.content'),
-
-        ]);
+    public function update(Article $article,SaveArticleRequest $request): ArticleResource
+    {
+        $article->update($request->validated());
 
         return ArticleResource::make($article);
+    }
+
+
+
+    public function destroy(Article $article): Response
+    {
+
+        $article->delete();
+
+        return response()->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArticleController;
 
-Route::get('articles', [ArticleController::class,'index'])->name('api.v1.articles.index');
-Route::get('articles/{article}', [ArticleController::class,'show'])->name('api.v1.articles.show');
-Route::post('articles', [ArticleController::class,'store'])->name('api.v1.articles.store');
-Route::patch('articles/{article}', [ArticleController::class,'update'])->name('api.v1.articles.update');
+Route::apiResource('articles', ArticleController::class)->names('api.v1.articles');
